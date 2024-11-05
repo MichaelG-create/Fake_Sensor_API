a0e063bc7326f1b1231c4296ba515eac56cfbaf4
Michael-pc <michael.garcia.73@gmail.com>
2024-11-05 18:40:45 +0100

    enhance simulate_visit_count method with :  _modulate_with_week_days  _add day selection feature with sys.argv  updated requirements.txt

--- sensor_app.py
+++ sensor_app.py
@@ -1,6 +1,10 @@
-from datetime import datetime
-import random
-# import numpy as np
+from datetime import date
+# date : useful when full days are used
+# datetime : useful when moment of the day are needed
+import sys
+
+import numpy as np
+
 
 
 class Sensor(object):
@@ -9,37 +13,63 @@ class Sensor(object):
         simulates a real visitor counter v in a mall
         returns a simulated visit_counts (at a given date)
     Attributes:
-        - average_visit_count
-        - standard_deviation_visit_count
+        - average_visit
+        - std_visit
     Methods:
-        - simulate_visit_count(average_visit_count, std_visit_count) -> int
+        - simulate_visit_count(average_visit, std_visit_count) -> int
     """
     def __init__(self, average_visit_count:int, standard_deviation_visit_count:int):
-        self.average_visit_count = average_visit_count
-        self.standard_deviation_visit_count = standard_deviation_visit_count
+        self.average_visit = average_visit_count
+        self.std_visit = standard_deviation_visit_count
 
-    def simulate_visit_count(self,date_visit:datetime)->int:
+    @staticmethod
+    def modulate_with_week_day(visit_count:int, visit_date:date)->int:
         """
-        Generate a number representing the visitor count at a particular date_visit
+        Modulate random visit_count to take into account weekday traffic increase
+        Monday 0    -> keep it
+        Tuesday 1   -> keep it
+        Wednesday 2 -> + 10 %
+        Thursday 3  -> keep it
+        Friday 4    -> + 25 %
+        Saturday 5  -> + 35 %
+        Sunday 6    -> closed   -> -1
+        :param visit_count:
+        :param visit_date:
+        :return:
+        """
+        week_day = visit_date.weekday()
+        if week_day == 2: visit_count *= 1.1
+        elif week_day == 4: visit_count *= 1.1
+        elif week_day == 5: visit_count *= 1.1
+        elif week_day == 6: visit_count = -1
+
+        return int(visit_count)
+
+    def simulate_visit_count(self,visit_date:date)->int:
+        """
+        Generate a number representing the visitor count at a particular visit_date
         Generation of the number :
         - random from
             - a normal distribution centered on:
-                - mean: self.average_visit_count
-                - standard deviation: self.standard_deviation_visit_count
+                - mean: self.average_visit
+                - standard deviation: self.std_visit
 
-        :param date_visit:
+        :param visit_date:
         :return:
         """
         # ensure reproducibility for the same day
-        random.seed(int(date_visit.timestamp()))
+        np.random.seed(visit_date.toordinal())
 
         # generate random visit count with random.normalvariate
-        visit_count = int(random.normalvariate(
-            self.average_visit_count,
-            self.standard_deviation_visit_count
+        visit_count = int(np.random.normal(
+            self.average_visit,
+            self.std_visit
         ))
 
-        print(f"Visits count was :{visit_count} this day : {date_visit}")
+        # add week_day impact on visit_count
+        visit_count = self.modulate_with_week_day(visit_count, visit_date)
+
+        print(f"Visits count was :{visit_count} this day : {visit_date}")
         return visit_count
 
 if __name__ == "__main__":
@@ -48,7 +78,11 @@ if __name__ == "__main__":
     # (without the .py extension).
     avg_visit_count = 2000
     std_visit_count = 100
-    date_of_visit = datetime(2024,11,5)
+    if len(sys.argv) > 1:
+        date_tok = sys.argv[1].split('-')
+        date_of_visit = date(int(date_tok[0]), int(date_tok[1]), int(date_tok[2]))
+    else :
+        date_of_visit = date(2024,11,5)
     sensor = Sensor(avg_visit_count, std_visit_count)
     result = sensor.simulate_visit_count(date_of_visit)
 
