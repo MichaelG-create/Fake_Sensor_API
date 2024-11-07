fd0fd421e656c55e54768feaeb288c72d4bddc00
Michael-pc <michael.garcia.73@gmail.com>
2024-11-07 21:04:05 +0100

    added simulate_store_visit_count Store class (multiple sensors in a store) -blacked all py TODO : -test store class -pylint all py -add CI -deploy with FASTApi -updated sensor_class with sensor_id (really needed ?)

--- sensor_app.py
+++ sensor_app.py
@@ -1,9 +1,15 @@
+"""
+Module containing Sensor Class
+"""
 from datetime import date
+
 # date : useful when full days are used
 # datetime : useful when moment of the day are needed
 import sys
 
 import numpy as np
+
+
 
 class Sensor(object):
     """
@@ -16,19 +22,23 @@ class Sensor(object):
         - malfunction_rate : probability  of malfunction of a sensor
         - break_rate  : probability  of breaking of a sensor
     Methods:
-        - simulate_visit_count(average_visit, std_visit_count) -> int
+        - simulate_visit_count(average_visit, STD_VISIT_COUNT) -> int
     """
-    def __init__(self,
-                 average_visit_count:int, standard_deviation_visit_count:int,
-                 malfunction_rate: float, break_rate: float
-                 ):
+
+    def __init__(
+        self,
+        average_visit_count: int,
+        standard_deviation_visit_count: int,
+        malfunction_rate: float,
+        break_rate: float,
+    ):
         self.average_visit = average_visit_count
         self.std_visit = standard_deviation_visit_count
 
         self.malfunction_rate = malfunction_rate
         self.break_rate = break_rate
 
-    def malfunction_or_break(self, visit_count:int)-> (int, str):
+    def malfunction_or_break(self, visit_count: int) -> (int, str):
         """
         if malfunction : diminish visit_count by 80% !
         if break event : visit_count = 0
@@ -39,17 +49,17 @@ class Sensor(object):
 
         if event <= self.break_rate:
             break_or_malfunction_flag = "break"
-            visit_count =  0
+            visit_count = 0
         elif event <= self.malfunction_rate:
             break_or_malfunction_flag = "malfunction"
-            visit_count =  int(visit_count * 0.2)
+            visit_count = int(visit_count * 0.2)
         else:
-            break_or_malfunction_flag = ''
+            break_or_malfunction_flag = ""
 
         return visit_count, break_or_malfunction_flag
 
     @staticmethod
-    def modulate_with_week_day(visit_count:int, visit_date:date)->int:
+    def modulate_with_week_day(visit_count: int, visit_date: date) -> int:
         """
         Modulate random visit_count to take into account weekday traffic increase
         Monday 0    -> keep it
@@ -64,14 +74,18 @@ class Sensor(object):
         :return:
         """
         week_day = visit_date.weekday()
-        if week_day == 2: visit_count *= 1.1
-        elif week_day == 4: visit_count *= 1.1
-        elif week_day == 5: visit_count *= 1.1
-        elif week_day == 6: visit_count = -1
+        if week_day == 2:
+            visit_count *= 1.1
+        elif week_day == 4:
+            visit_count *= 1.1
+        elif week_day == 5:
+            visit_count *= 1.1
+        elif week_day == 6:
+            visit_count = -1
 
         return int(visit_count)
 
-    def simulate_visit_count(self,visit_date:date)->int:
+    def simulate_visit_count(self, visit_date: date) -> int:
         """
         Generate a number representing the visitor count at a particular visit_date
         Generation of the number :
@@ -87,10 +101,7 @@ class Sensor(object):
         np.random.seed(visit_date.toordinal())
 
         # generate random visit count with random.normalvariate
-        visit_count = int(np.random.normal(
-            self.average_visit,
-            self.std_visit
-        ))
+        visit_count = int(np.random.normal(self.average_visit, self.std_visit))
 
         # add week_day impact on visit_count
         visit_count = self.modulate_with_week_day(visit_count, visit_date)
@@ -98,25 +109,24 @@ class Sensor(object):
         # malfunction or break event
         visit_count, flag = self.malfunction_or_break(visit_count)
 
-
-        print(f"Visits count was :{visit_count} this day : {visit_date} "+flag)
+        print(f"Visits count was :{visit_count} this day : {visit_date} " + flag)
         return visit_count
+
 
 if __name__ == "__main__":
     # meaning : do this only if the script is directly run (__name__ = __main__')
     # When it’s imported as a module in another file, __name__ is set to the name of the file/module
     # (without the .py extension).
-    avg_visit_count = 2000
-    std_visit_count = 100
+    AVG_VISIT_COUNT: int = 2000
+    STD_VISIT_COUNT: int = 100
 
-    malfunction_chance = 0.035
-    break_chance = 0.015
+    MALFUNCTION_CHANCE: float = 0.035
+    BREAK_CHANCE: float = 0.015
 
     if len(sys.argv) > 1:
-        date_tok = sys.argv[1].split('-')
+        date_tok = sys.argv[1].split("-")
         date_of_visit = date(int(date_tok[0]), int(date_tok[1]), int(date_tok[2]))
-    else :
-        date_of_visit = date(2024,11,5)
-    sensor = Sensor(avg_visit_count, std_visit_count, malfunction_chance, break_chance)
+    else:
+        date_of_visit = date(2024, 11, 5)
+    sensor = Sensor(AVG_VISIT_COUNT, STD_VISIT_COUNT, MALFUNCTION_CHANCE, BREAK_CHANCE)
     result = sensor.simulate_visit_count(date_of_visit)
-
--- test_sensor.py
+++ test_sensor.py
@@ -16,34 +16,47 @@ class TestSensor(unittest.TestCase):
         malfunction_chance = 0.035
         break_chance = 0.015
 
-        self.sensor = Sensor(avg_visit_count, std_visit_count, malfunction_chance, break_chance)
+        self.sensor = Sensor(
+            avg_visit_count, std_visit_count, malfunction_chance, break_chance
+        )
 
     # def tearDown(self):
     #     # This method is run after each test
     #     print("Cleaning up after test")
 
-    def test_closed_on_sunday(self)->None:
-        sunday_date = date( 2024 ,11 ,3 )
-        self.assertTrue( -1 == self.sensor.simulate_visit_count(sunday_date),
-        f"error : opened this sunday : {sunday_date} with {self.sensor.simulate_visit_count(sunday_date)} visitors")
+    def test_closed_on_sunday(self) -> None:
+        sunday_date = date(2024, 11, 3)
+        self.assertTrue(
+            -1 == self.sensor.simulate_visit_count(sunday_date),
+            f"error : opened this sunday : {sunday_date} with {self.sensor.simulate_visit_count(sunday_date)} visitors",
+        )
 
     # needs to be tested over a set of weekday date (monday, tuesday, wednesday, thursday, friday, saturday
     def test_opened_on_worked_days(self):
-        weekday_list = pd.date_range(date(2024,11,4), date(2024,11,9))
+        weekday_list = pd.date_range(date(2024, 11, 4), date(2024, 11, 9))
         for weekday_date in weekday_list:
-            self.assertTrue( -1 != self.sensor.simulate_visit_count(weekday_date),
-            f"error : should be opened this weekday : {weekday_date} but got {self.sensor.simulate_visit_count(weekday_date)} visitors")
+            self.assertTrue(
+                -1 != self.sensor.simulate_visit_count(weekday_date),
+                f"error : should be opened this weekday : {weekday_date} but got {self.sensor.simulate_visit_count(weekday_date)} visitors",
+            )
 
-    def test_malfunction_day(self)->None:
-        malfunction_date = date(2022, 2 , 17)
-        self.assertTrue( 401 == self.sensor.simulate_visit_count(malfunction_date),
-        (f"error : should malfunction this days {malfunction_date} "
-         f"but got {self.sensor.simulate_visit_count(malfunction_date)} visitors instead of 401"))
+    def test_malfunction_day(self) -> None:
+        malfunction_date = date(2022, 2, 17)
+        self.assertTrue(
+            401 == self.sensor.simulate_visit_count(malfunction_date),
+            (
+                f"error : should malfunction this days {malfunction_date} "
+                f"but got {self.sensor.simulate_visit_count(malfunction_date)} visitors instead of 401"
+            ),
+        )
 
-    def test_break_day(self)->None:
-        break_date = date(2022, 2, 4 )
-        self.assertTrue( 0 == self.sensor.simulate_visit_count(break_date),
-        f"error : should be broken this day : {break_date}, got {self.sensor.simulate_visit_count(break_date)} visitors instead of 0")
+    def test_break_day(self) -> None:
+        break_date = date(2022, 2, 4)
+        self.assertTrue(
+            0 == self.sensor.simulate_visit_count(break_date),
+            f"error : should be broken this day : {break_date}, got {self.sensor.simulate_visit_count(break_date)} visitors instead of 0",
+        )
+
 
 if __name__ == "__main__":
     # meaning : do this only if the script is directly run (__name__ = __main__')
@@ -52,4 +65,3 @@ if __name__ == "__main__":
 
     # use this to run the tests with the above created class
     unittest.main()
-
